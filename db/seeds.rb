0db038c1d104c19a77ade5214d16d0f9a393ce9b
ujkhokhar <ujkhokhar@gmail.com>
2015-03-26 18:05:34 -0400

    Add seed data for users and wikis.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+## Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save
+end
+
+## Create admin user for testing
+umar = User.create!(
+  name: 'Umar Khokhar',
+  email: 'ujkhokhar@gmail.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+umar.skip_confirmation!
+umar.save
+
+users = User.all
+puts "#{User.count} users created."
+
+## Create wikis
+20.times do 
+  wiki = Wiki.create!(
+    user: users.sample,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+wikis = Wiki.all
+puts "#{wikis.count} wikis created."
