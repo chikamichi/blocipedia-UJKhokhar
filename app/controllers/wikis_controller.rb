04742eec8825973f914ab3dd1ffd7ec94006ba3b
ujkhokhar <ujkhokhar@gmail.com>
2015-03-26 03:25:34 -0400

    Add wikis#create.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -11,6 +11,23 @@ class WikisController < ApplicationController
     @wiki = Wiki.new
   end
 
+  def create
+    @wiki = Wiki.create(wiki_params)
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error creating your wiki."
+      redirect_to :new
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body)
   end
 end
