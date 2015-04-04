83b328a6e4e448e00b9ff25e0d2798c828455773
ujkhokhar <ujkhokhar@gmail.com>
2015-04-04 18:06:42 -0400

    Wiki has many collaborators through users.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,5 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+  has_many :collaborators, through: :users
   scope :visible_to, ->(user, viewable = true) { user ? all : where(public: viewable) }
 end
