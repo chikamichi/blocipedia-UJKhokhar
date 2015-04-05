4c97439d1e8c5b939901de23e1b9befbd04189d4
ujkhokhar <ujkhokhar@gmail.com>
2015-04-04 20:59:55 -0400

    Adjust collaborators table. Add collaborator_id to wikis.

--- db/migrate/20150404044923_create_collaborators.rb
+++ db/migrate/20150404044923_create_collaborators.rb
@@ -1,14 +1,13 @@
 class CreateCollaborators < ActiveRecord::Migration
   def change
     create_table :collaborators do |t|
-      t.integer :user_id
-      t.integer :wiki_id
+      t.string :name
       t.timestamps
     end
 
-    add_index :collaborators, :id, unique: true
-    add_index :collaborators, :user_id
-    add_index :collaborators, :wiki_id
+    add_column :wikis, :collaborator_id, :integer
+    add_index :wikis, :collaborator_id
+
   end
 
 end
