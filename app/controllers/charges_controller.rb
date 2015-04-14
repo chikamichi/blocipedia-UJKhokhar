cdcf3ca3aed9e757348c698c2c2dba1a979e2a15
ujkhokhar <ujkhokhar@gmail.com>
2015-04-14 03:35:44 -0400

    Change one time charge to subscription. Add downgrade action.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -11,19 +11,17 @@ class ChargesController < ApplicationController
 
   def create
     @amount = 15_00
+    
+    if current_user.customer_id.nil?
+      customer = Stripe::Customer.create(
+        email: current_user.email,
+        card: params[:stripeToken]
+      )
+      current_user.customer_id = customer.id
+    end
 
-    customer = Stripe::Customer.create(
-      email: current_user.email,
-      card: params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @amount,
-      description: "BigMoney Membership - #{current_user.email}",
-      currency: 'usd'
-    )
-
+    customer = Stripe::Customer.retrieve(current_user.customer_id)
+    customer.subscriptions.create(plan: 'premium')
     current_user.update_attribute(:role, 'premium')
 
     flash[:success] = "Your payment has been receieved. Thank you!"
@@ -33,6 +31,13 @@ class ChargesController < ApplicationController
     current_user.update_attribute(:role, 'standard')
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
+
+  def downgrade
+    customer = Stripe::Customer.retrieve(current_user.customer_id)
+    subscription = customer.subscriptions.data.first
+    customer.subscriptions.retrieve(subscription).delete
+    current_user.update_attributes(role: 'standard')
   end
 
 end
