ea771ca0dbf63092e9ce36fc16f262548d236d75
ujkhokhar <ujkhokhar@gmail.com>
2015-04-15 04:00:36 -0400

    Users private wikis are public after downgrading

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -40,6 +40,7 @@ class SubscriptionsController < ApplicationController
     subscription_id = customer.subscriptions.data.first.id
     customer.subscriptions.retrieve(subscription_id).delete
     current_user.update_attributes(role: 'standard')
+    current_user.make_wikis_public
     redirect_to edit_user_registration_path
   end
 
